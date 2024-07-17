2bee4b8a134bd0843a088d0ee65355d784f15d85
Daehyun Jung <daehyunj1997@gmail.com>
2024-07-16 17:16:50 -0700

    Feature Header Set up

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,11 +1,18 @@
+import { FaReddit, FaSearch } from "react-icons/fa";
+import { Link } from "react-router-dom";
+
+export default function Header() {
+  return (
+    <nav className="flex justify-between items-center py-8 container mx-auto flex-col lg:flex-row gap-5 lg:gap-0">
+      <Link to={'/'}>
+      <div className="ml-5">
+        <FaReddit />
+        <h2 className="font-semibold text-3xl text-red-bold">Reddit</h2>
+      </div>
+      </Link>
 
 
 
-
-
-export default function Header(){
-
-    return <div>
-        Header
-    </div>
+    </nav>
+  );
 }
