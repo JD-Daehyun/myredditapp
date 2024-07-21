d4cff999d4a6f99f385809a41004b0496b1a9e68
Daehyun Jung <daehyunj1997@gmail.com>
2024-07-20 17:42:01 -0700

    Fix Comment bug size overflow

--- src/components/comment/index.jsx
+++ src/components/comment/index.jsx
@@ -4,7 +4,7 @@ export default function Comment({ comment }) {
   return (
     <div className="flex flex-col m-2">
         <p className="font-semibold mb-2">{comment?.data.author}</p>
-      <ReactMarkdown className='p-2 bg-gray-200 rounded-lg max-w-[1000px]'>{comment?.data.body}</ReactMarkdown>
+      <ReactMarkdown className='p-2 bg-gray-200 rounded-lg max-w-[1000px] w-fit'>{comment?.data.body}</ReactMarkdown>
     </div>
   );
 }
